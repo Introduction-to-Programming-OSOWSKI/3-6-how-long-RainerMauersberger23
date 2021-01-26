7d704decc09f8d35cb5e3dd25ad4e5f1724accc2
RainerMauersberger23 <829532@mystma.org>
2021-01-26 05:44:04 +0000

    howLong Saved

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-#WRITE YOUR CODE IN THIS FILE
+def howLong(w):
+   
+    for i in range(0, len(w)):
+        if w[i] == "1":
+
+            return True
+
+    return False 
+
+print (howLong("star wars"))
